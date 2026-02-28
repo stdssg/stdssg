4fca85581663b1e6e794ca8482dc68c8683edfe9
Nishant Chhillar <nchhillar2004@gmail.com>
2026-02-28 13:52:45 +0530

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+const char nl = '\n';
+
+int main(int argc, char* argv[]) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::cout << "stdssg v1.0.0" << nl;
+
+    return 0;
+}
